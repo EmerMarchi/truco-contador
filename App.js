7a0c9922d4344806e738c4e1d66357a42bcf5e33
Emerson Jean <marchimeson123@gmail.com>
2026-03-16 20:48:43 -0300

    Criando função aumentar

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const[contador, setContador] = useState(0)
+
+  function aumentar(){
+    setContador(contador + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Teste</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
