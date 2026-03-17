0106769a536207208f668ff2f19836ac59b12d8e
Emerson Jean <marchimeson123@gmail.com>
2026-03-17 20:07:44 -0300

    Primeiro marcador concluido

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   
@@ -10,10 +10,25 @@ export default function App() {
     setContador(contador + 1)
   }
 
+  function diminuir(){
+    setContador(contador - 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Teste</Text>
-      <StatusBar style="auto" />
+      <Image source={require('./assets/logo.png')} />
+      <Text style={styles.marcador}>MARCADOR</Text>
+      <Text style={styles.numero}>{contador}</Text>
+
+    <View style={styles.buttonsContainer}>
+        <View style={styles.buttons}>
+          <Button color='green' onPress={aumentar} title='+' />
+        </View>
+        <View style={styles.buttons}>
+          <Button color='red' onPress={diminuir} title='-' />
+        </View>
+      </View>
+      
     </View>
   );
 }
@@ -25,4 +40,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonsContainer:{
+    flexDirection: 'row',
+    gap:40
+  },
+  marcador:{
+    fontSize:30,
+    marginBottom:20,
+    fontWeight:"bold"
+  },
+  numero:{
+    fontSize:60,
+    marginBottom:30
+  },
+  buttons:{
+    width:60,
+    height:40,
+  },
+  
 });
